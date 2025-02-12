88cb74c1a4a9c6b55bed80037109af7efd63792a
buz galbriath <woodward.b.galbraith@kp.org>
2025-02-11 19:25:23 -0800

    updating code up to start of run

--- mapnet/dev/get_doid_known_maps.py
+++ mapnet/dev/get_doid_known_maps.py
@@ -30,7 +30,7 @@ for iri in doid.owl_classes.keys():
         if annotation.startswith("MESH:"):
             provided_maps[iri] = mesh_map(annotation.split(" ")[0])
 
-with open("./doid_to_mesh_provided_maps.tsv", mode='w') as f:
-    f.write("DOID_URI\tMESH_URI\tSCORE\n")
+with open("knownMaps/doid_to_mesh_provided_maps.tsv", mode='w') as f:
+    f.write("SrcEntity\tTgtEntity\tScore\n")
     for doid_iri in provided_maps:
         f.write(f"{doid_iri}\t{provided_maps[doid_iri]}\t1.0\n")
