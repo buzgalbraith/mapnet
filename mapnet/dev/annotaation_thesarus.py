3ecb00a85388f2144eba7b45e225a70e20c568aa
buz galbraith <buzgalbraith@gmail.com>
2025-03-05 10:28:34 -0800

    refactored filtering a bit

---
+++ mapnet/dev/annotaation_thesarus.py
@@ -0,0 +1,16 @@
+from deeponto.onto import Ontology
+from deeponto.align.bertmap import DEFAULT_CONFIG_FILE, BERTMapPipeline
+
+## setup betmap config
+config = BERTMapPipeline.load_bertmap_config(DEFAULT_CONFIG_FILE)
+config.known_mappings = "knownMaps/doid_to_mesh_provided_maps.tsv"
+config.output_path = "/home/buzgalbraith/workspaace/mapnet/"
+# config.global_matching.enabled = True
+# config.bert.resume_training = True
+## set up ontologies
+src_onto_file = "resources/doid.owl"
+tgt_onto_file = "resources/mesh_primary_disseae.owl"
+src_onto = Ontology(src_onto_file)
+tgt_onto = Ontology(tgt_onto_file)
+
+BERTMapPipeline(src_onto, tgt_onto, config)
