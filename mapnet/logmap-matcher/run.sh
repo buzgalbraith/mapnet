0195801e43aa554dac5d76dc7a09def9cdba9f2e
buz galbraith <buzgalbraith@gmail.com>
2025-04-09 14:53:08 -0400

    adding base for logmap code

---
+++ mapnet/logmap-matcher/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh  
+java -jar \
+    --add-opens java.base/java.lang=ALL-UNNAMED \
+    /package/logmap/logmap-matcher-4.0.jar \
+    MATCHER \
+    file:///package/resources/doid.owl \
+    file:///package/resources/mesh_primary_disseae.owl \
+    /package/output/ \
+    false ## classify input ontologies along with mapping?
+
