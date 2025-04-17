c20862ef49234afeb6f8992734c856f8eaa2d94a
buz galbraith <buzgalbraith@gmail.com>
2025-04-17 15:50:55 -0400

    fixing import issue

--- mapnet/logmap-matcher/matcher_run.sh
+++ mapnet/logmap-matcher/matcher_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 # download ontologies
-# echo "16g" | python3 -c 'from mapnet.bertmap.example import data_setup; data_setup()'
+echo "16g" | python3 -c 'from mapnet.bertmap import data_setup; data_setup()'
 
 ## build image 
 docker build -f mapnet/logmap-matcher/Dockerfile ./ -t logmap:0.01
@@ -11,4 +11,4 @@ echo "running matching"
 docker run --rm -v ./mapnet/logmap-matcher/output:/package/output logmap:0.01 sh run.sh
 
 ## quality check matches and write the results 
-# python3 mapnet/logmap-matcher/matching_qc.py
+python3 mapnet/logmap-matcher/matching_qc.py
